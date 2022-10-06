70c6d3399f4e17bfe1c5e78d6a6ad2622ae7afa9
Pedro Franca <pedrodepaulafranca@gmail.com>
2022-10-06 15:55:07 -0300

    cria rota para listar produtos

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -6,5 +6,6 @@ const router = Router();
 const productController = new ProductController();
 
 router.post('/', productController.insert);
+router.get('/', productController.getAll);
 
 export default router;
