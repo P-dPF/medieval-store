c0dd6eeb2d1b5439147d82f6919b0f06e2bccfed
Pedro Franca <pedrodepaulafranca@gmail.com>
2022-10-07 14:55:12 -0300

    atualiza controller de login para incluir validação joi na função de fazer login

--- src/controllers/login.controller.ts
+++ src/controllers/login.controller.ts
@@ -1,10 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
 import LoginService from '../services/login.service';
 import statusCodes from '../utils/statusCodes';
-import validateSchema from '../validations/validateSchemas';
-import loginSchema from '../validations/schemas';
-import errorGenerator from '../utils/errorGenerator';
+import { loginSchema } from '../validations/schemas';
 import IUser from '../interfaces/user.interface';
+import joiInputValidate from '../validations/joi.input.validation';
+import errorGenerator from '../utils/errorGenerator';
 
 export default class LoginController {
   constructor(private service = new LoginService()) { }
@@ -12,13 +12,12 @@ export default class LoginController {
   public loginHandler = async (req: Request, res: Response, next: NextFunction) => {
     const login = req.body;
 
-    const error = validateSchema<IUser>(loginSchema, login);
-    
-    if (error) return next(errorGenerator(statusCodes.BAD_REQUEST, error.message));
+    const joiError = joiInputValidate<IUser>(loginSchema, login);
+    if (joiError) return next(joiError);
 
     const token = await this.service.loginHandler(login.username, login.password);
     if (!token) {
-      return res.status(statusCodes.UNAUTHORIZED).json({ message: 'Username or password invalid' });
+      return next(errorGenerator(statusCodes.UNAUTHORIZED, 'Username or password invalid'));
     }
     res.status(statusCodes.OK).json({ token });
   };
