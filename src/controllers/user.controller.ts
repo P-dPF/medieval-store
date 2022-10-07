69f977387727f1fe6aeadcff1dee82e87235910b
Pedro Franca <pedrodepaulafranca@gmail.com>
2022-10-07 15:12:48 -0300

    atualiza controller de usuario para incluir validação joi

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,12 +1,18 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
+import IUser from '../interfaces/user.interface';
 import UserService from '../services/user.service';
 import statusCodes from '../utils/statusCodes';
+import joiInputValidate from '../validations/joi.input.validation';
+import { addNewUserSchema } from '../validations/schemas';
 
 export default class UserController {
   constructor(private service = new UserService()) { }
 
-  public insert = async (req: Request, res: Response) => {
+  public insert = async (req: Request, res: Response, next: NextFunction) => {
     const user = req.body;
+
+    const joiError = joiInputValidate<IUser>(addNewUserSchema, user);
+    if (joiError) return next(joiError);
 
     const token = await this.service.insert(user);
     res.status(statusCodes.CREATED).json({ token });
