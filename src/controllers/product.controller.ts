6df2b0aa9bdc8c0a87053fbaa0c6dc281cbcf3f8
Pedro Franca <pedrodepaulafranca@gmail.com>
2022-10-07 14:54:48 -0300

    atualiza controller de produto para incluir validação joi no função de incluir produto

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,13 +1,19 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import statusCodes from '../utils/statusCodes';
 import ProductService from '../services/product.service';
+import { addProductSchema } from '../validations/schemas';
+import IProduct from '../interfaces/product.interface';
+import joiInputValidate from '../validations/joi.input.validation';
 
 export default class ProductController {
   constructor(private service = new ProductService()) { }
 
-  public insert = async (req: Request, res: Response) => {
+  public insert = async (req: Request, res: Response, next: NextFunction) => {
     const product = req.body;
 
+    const joiError = joiInputValidate<IProduct>(addProductSchema, product);
+    if (joiError) return next(joiError);
+    
     const insertedProduct = await this.service.insert(product);
     res.status(statusCodes.CREATED).json(insertedProduct);
   };
